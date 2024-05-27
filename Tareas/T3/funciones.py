83da8097002dbd6bded2646b3d76e3e51edb805b
Trinidad Campos <trinidad.campos@uc.cl>
2024-05-27 10:28:26 -0400

    arreglo carga base :question:

--- Tareas/T3/funciones.py
+++ Tareas/T3/funciones.py
@@ -1,28 +1,46 @@
 from utilidades import Animales, Candidatos, Distritos, Locales, Votos, Ponderador
 
 
-def arreglo(algo):
-    if algo.__class__ != list:
-        if algo.isdigit():
-            algo = int(algo)
-        elif "." in algo:
-            algo = float(algo)
+def arreglo(algo, tipo):
+    if tipo == "lista":
+        for i in range(len(algo)):
+            algo[i] = int(algo[i])
+        return algo
+    else:
+        algo[0] = int(algo[0])
+        algo[2] = int(algo[2])
         return algo
 
 
 def cambio(lista, tipo):
     if tipo == "animales":
+        lista[0] = int(lista[0])
+        lista[3] = int(lista[3])
+        lista[4] = float(lista[4])
+        lista[5] = int(lista[5])
         return Animales(*lista)
+
     elif tipo == "candidatos":
+        lista[0] = int(lista[0])
+        lista[2] = int(lista[2])
         return Candidatos(*lista)
+
     elif tipo == "distritos":
+        lista[0] = int(lista[0])
+        lista[2] = int(lista[2])
         return Distritos(*lista)
+
     elif tipo == "ponderadores":
+        lista[1] = float(lista[1])
         return Ponderador(*lista)
+
     elif tipo == "votos":
+        lista[0] = int(lista[0])
+        lista[1] = int(lista[1])
+        lista[2] = int(lista[2])
+        lista[3] = int(lista[3])
         return Votos(*lista)
-    elif tipo == "distritos":
-        return Distritos(*lista)
+
     elif tipo == "locales":
         return Locales(*lista)
 
