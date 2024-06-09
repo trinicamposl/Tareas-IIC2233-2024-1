41dd99eaab15af5e4785a9bbea870b9b0aee01de
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-09 13:29:02 -0400

    pruebas puntajes y nuevos parámetros

---
+++ Tareas/T4/cliente/parametros.py
@@ -0,0 +1,12 @@
+from os import path
+IMAGEN_PATH = path.join("assets", "sprites", "logo.png")
+ANCHO_IMAGEN = 300
+ALTURA_IMAGEN = 100
+ANCHO_JUEGO = 600
+ALTURA_JUEGO = 480
+PATH_MUSICA_FONDO = path.join("assets", "sonidos", "musica_1.wav")
+TIEMPO_APARICION = 45
+TIEMPO_DURACION = 10
+TIEMPO_ADICIONAL = 35
+lista_tiempos = [["novato", 60], ["intermedio", 300 ], ["experto", 600]]
+TIEMPO_JUEGO = {i[0]:i[1] for i in lista_tiempos}
