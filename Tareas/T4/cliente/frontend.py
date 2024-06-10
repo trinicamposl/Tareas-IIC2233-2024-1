2570c290b7dcaafcf0c400f01b6467f230f5d485
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-10 01:32:28 -0400

    tiempo :timer_clock:

--- Tareas/T4/cliente/frontend.py
+++ Tareas/T4/cliente/frontend.py
@@ -1,9 +1,8 @@
 from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
-from PyQt6.QtGui import QFont, QPixmap, QMouseEvent, QKeyEvent, QFont, QFontDatabase
-from PyQt6.QtCore import Qt, pyqtSignal, QUrl
+from PyQt6.QtGui import QFont, QPixmap, QMouseEvent, QKeyEvent
+from PyQt6.QtCore import Qt, pyqtSignal, QUrl, QTimer
 from PyQt6.QtWidgets import QHBoxLayout, QWidget, QComboBox, QFormLayout, QLineEdit, QGridLayout
 from PyQt6.QtWidgets import QLabel, QPushButton, QScrollArea, QVBoxLayout, QApplication
-from os import path
 import parametros as p
 from funciones import archivos, salon_fama, diccionario
 import sys
@@ -158,6 +157,30 @@ class LechugasIniciales(QWidget):
             if columna > tamano:
                 columna = 0
                 fila += 1
+
+
+class Tiempo(QWidget):
+    def __init__(self, nivel: str):
+        super().__init__()
+        self.duration = p.TIEMPO_JUEGO[nivel.split("_")[0]]
+        self.label2 = QLabel()
+        self.label2.setStyleSheet('border: 1px solid black')
+        self.label2.setText(f'Te quedan {self.duration} segundos.')
+        self.label2.show()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(1000)
+
+    def update(self):
+        self.label2.setText(f'Te quedan {self.duration} segundos.')
+        self.duration -= 1
+
+        if self.duration < 0:
+            self.timer.stop()
+            exit()  # perdiste!
+
+    def aumento(self, cuanto):
+        pass
 
 
 class VentanaJuego(QWidget):
@@ -316,6 +339,6 @@ if __name__ == '__main__':
     # a = QFontDatabase.addApplicationFont(p.PATH_LETRA)
     font = QFont("Cascadia Mono SemiBold", 12)
     app.setFont(font)  # Creamos las base de la app: QApplication.
-    ventana = LechugasIniciales("intermedio_1.txt")   # Construimos un QWidget que será nuestra ventana.
+    ventana = Tiempo("intermedio_1.txt")   # Construimos un QWidget que será nuestra ventana.
     ventana.show()  # Mostramos la ventana.
     sys.exit(app.exec())
