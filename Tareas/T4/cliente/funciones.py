2f1e5cb24ecd05953dd9f4c175b4d4e248d290c0
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-10 00:36:23 -0400

    diccionarios :place_of_worship:

--- Tareas/T4/cliente/funciones.py
+++ Tareas/T4/cliente/funciones.py
@@ -15,3 +15,31 @@ def salon_fama():
         for elemento in puntajes:
             nueva.append(f"{elemento[0]} - {elemento[1]}")
         return nueva
+
+
+def diccionario(nivel):
+    ruta = path.join("assets", "base_puzzles", nivel)
+    with open(ruta, "r", encoding="UTF-8") as archivo:
+        columna = archivo.readline().strip().split(";")
+        fila = archivo.readline().strip().split(";")
+        for i in range(len(fila)):
+            if fila[i] == "-":
+                fila[i] = ""
+            else:
+                linea = fila[i].split(",")
+                fila[i] = " ".join(linea)
+
+        for i in range(len(columna)):
+            if columna[i] == "-":
+                columna[i] = ""
+            else:
+                linea = columna[i].split(",")
+                columna[i] = "\n".join(linea)
+
+        diccionario = {}
+        for i in range(len(columna)):
+            diccionario["0,"+str(i + 1)] = columna[i]
+
+        for i in range(len(fila)):
+            diccionario[str(i + 1)+",0"] = fila[i]
+        return diccionario
