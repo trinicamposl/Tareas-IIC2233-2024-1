650fa440845d0e3034d25fdebcbeaca00c6cd4cf
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-13 17:49:07 -0400

    arreglos parámetros varios :recycle:

--- Tareas/T4/cliente/frontend/parametros.py
+++ Tareas/T4/cliente/frontend/parametros.py
@@ -13,8 +13,8 @@ lista_tiempos = [["novato", 60], ["intermedio", 300], ["experto", 600]]
 TIEMPO_JUEGO = {i[0]: i[1] for i in lista_tiempos}
 
 LECHUGA_PATH = path.join("..", "assets", "sprites", "lechuga.png")
-ANCHO_LECHUGA = [200, 42, 42]
-ALTURA_LECHUGA = [65, 14, 14]
+ANCHO_LECHUGA = [20, 20, 20]
+ALTURA_LECHUGA = [20, 20, 20]
 
 lista_tamano = [["novato", 3], ["intermedio", 10], ["experto", 20]]
 TAMANO = {i[0]: i[1] for i in lista_tamano}
@@ -39,5 +39,10 @@ PATHS = [["abajo", [[0, path.join("..", "assets", "sprites", "pepa", "down_0.png
                       [2, path.join("..", "assets", "sprites", "pepa", "right_2.png")],
                       [3, path.join("..", "assets", "sprites", "pepa", "right_3.png")]]]]
 RUTAS = {i[0]: i[1] for i in PATHS}
+PASOS = [6, 6, 6]
 
-PASOS = [14, 6, 6]
+pantalla_ancho = [["novato", 300], ["intermedio", 550], ["experto", 900]]
+ANCHO_PANTALLA = {i[0]: i[1] for i in pantalla_ancho}
+
+pantalla_largo = [["novato", 200], ["intermedio", 450], ["experto", 650]]
+LARGO_PANTALLA = {i[0]: i[1] for i in pantalla_largo}
