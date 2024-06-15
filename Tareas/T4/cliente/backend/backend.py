d7d400406b75589fd80dba22aead57c54bdf1a39
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-15 19:29:42 -0400

    entendí las señales!

--- Tareas/T4/cliente/backend/backend.py
+++ Tareas/T4/cliente/backend/backend.py
@@ -1,13 +1,14 @@
 from PyQt6.QtCore import QObject, pyqtSignal
+import frontend.Tablero as tablero
 
 
 class Usuario(QObject):
-    signal_intentar_empezar = pyqtSignal(list)
-    signal_empezar = pyqtSignal(list)
+    signal_intentar_empezar = pyqtSignal(str)
+    signal_empezar = pyqtSignal(bool)
+    signal_datos = pyqtSignal(list)
 
-    def __init__(self, nivel):
+    def __init__(self):
         super().__init__()
-        self.nivel = nivel
         self.puede = False
 
     def revisar_texto(self, texto):
@@ -16,6 +17,17 @@ class Usuario(QObject):
                 if elemento in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
                     self.puede = True
         self.signal_empezar.emit(self.puede)
+
+    def guardar_datos(self, lista):
+        self.nombre = lista[0]
+        self.nivel = lista[1]
+        self.crear_tablero(self.nivel)
+
+    def crear_tablero(self, parametro):
+        crear_tablero = tablero.Tablero(parametro)
+        crear_tablero.show()
+
+
 
 # backend.signal_intentar_empezar.connect(revisar_texto)
 # frontend.signal_empezar.connect(recibir_info):
