45e8b9e59c575151a69721d2841a8f93b31c7614
Trinidad Campos <trinidad.campos@uc.cl>
2024-06-16 10:53:44 -0400

    existe el tiempo infinito!!! :infinity:

--- Tareas/T4/cliente/frontend/Tablero.py
+++ Tareas/T4/cliente/frontend/Tablero.py
@@ -1,6 +1,6 @@
-from PyQt6.QtGui import QPixmap, QKeyEvent
-from PyQt6.QtCore import pyqtSignal, QTimer, QThread, QMutex, Qt
-from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QVBoxLayout
+from PyQt6.QtGui import QPixmap, QKeyEvent, QFont, QShortcut, QKeySequence
+from PyQt6.QtCore import pyqtSignal, QTimer, Qt
+from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QVBoxLayout, QApplication
 import parametros as p
 from funciones import diccionario
 import sys
@@ -32,32 +32,38 @@ class Tiempo(QWidget):
 
     def aumento(self, cuanto):
         self.duration += cuanto
-
-
-class Thread(QThread):
-    fin = pyqtSignal()
-
-    def run(self):
-        self.fin.emit()
+    
+    def detener(self):
+        self.timer.stop()
 
 
 class Tablero(QWidget):
-    signal_izq = pyqtSignal(str)
-    signal_der = pyqtSignal(str)
-    signal_arriba = pyqtSignal(str)
-    signal_abajo = pyqtSignal(str)
+    signal_posicion = pyqtSignal(list)
+    signal_salir = pyqtSignal()
+    signal_agregar_pepa = pyqtSignal()
+    signal_izquierda = pyqtSignal()
+    signal_derecha = pyqtSignal()
+    signal_arriba = pyqtSignal()
+    signal_abajo = pyqtSignal()
+    signal_silenciar = pyqtSignal()
 
     def __init__(self, nivel: str):
+        self.nivel = nivel  # nombre completo ej: intermedio_1.txt
+        self.nivel_2 = nivel.split("_")[0]  # nombre dificultad
+        self.tamano = p.TAMANO[self.nivel_2]
+        self.dificultad = p.NIVEL[self.nivel_2]  # numero asociado a la dificultad, 1; 2 ó 3
+        self.grid_layout = None
+        self.cor_x = 0  # estas coordenadas indican exactamente dónde está pepe (pixeles)
+        self.cor_y = 0
 
-        nivel_2 = nivel.split("_")[0]
-        self.tamano = p.TAMANO[nivel_2]
-        self.dificultad = p.NIVEL[nivel_2]
         super().__init__()
         self.setGeometry(50, 50, 100, 100)
-        self.setFixedSize(p.ANCHO_PANTALLA[nivel_2], p.LARGO_PANTALLA[nivel_2])
+        self.setFixedSize(p.ANCHO_PANTALLA[self.nivel_2], p.LARGO_PANTALLA[self.nivel_2])
+        self.iniciar_dibujos()
+        self.instalar_atajos()
 
-        grid_layout = QGridLayout()
-        self.setLayout(grid_layout)
+    def iniciar_dibujos(self):
+        self.grid_layout = QGridLayout()
         fila = 0
         columna = 0
         for i in range((self.tamano+1)**2):
@@ -66,22 +72,29 @@ class Tablero(QWidget):
                 if columna == 0 and fila == 0:
                     vacio = QLabel(self)
                 else:
-                    vacio = QLabel(f"{diccionario(nivel)[datos]}", self)
-                grid_layout.addWidget(vacio, fila, columna)
+                    vacio = QLabel(f"{diccionario(self.nivel)[datos]}", self)
+                self.grid_layout.addWidget(vacio, fila, columna)
             else:
                 lechuga = QLabel(self)
                 lechuga.setPixmap(QPixmap(p.LECHUGA_PATH))
-                lechuga.setFixedSize(p.ANCHO_LECHUGA,
-                                     p.ALTURA_LECHUGA)
-                grid_layout.addWidget(lechuga, fila, columna)
+                lechuga.setFixedSize(p.ANCHO_LECHUGA, p.ALTURA_LECHUGA)
+                self.grid_layout.addWidget(lechuga, fila, columna)
 
             columna += 1
             if columna > self.tamano:
                 columna = 0
                 fila += 1
 
-        self.tiempo = Tiempo(nivel)
-        grid_layout.addWidget(self.tiempo, 0, self.tamano + 1)
+        self.pepa = QLabel(self)
+        self.pepa.setPixmap(QPixmap(p.RUTAS["abajo"][0]))
+        self.pepa.setGeometry(self.cor_x, self.cor_y, p.ANCHO_LECHUGA, p.ALTURA_LECHUGA)
+        self.pepa.setWindowFlags(self.pepa.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+        self.pepa.show()
+
+        QTimer.singleShot(100, self.definir_Pepa)
+
+        self.tiempo_original = Tiempo(self.nivel)
+        self.grid_layout.addWidget(self.tiempo_original, 0, self.tamano + 1)
         self.salir = QPushButton("Salir")
         self.salir.setFixedHeight(p.ALTURA_LECHUGA)
         self.comprobar = QPushButton("Comprobar")
@@ -92,113 +105,75 @@ class Tablero(QWidget):
 
         for i in range(self.tamano):
             if i == 1:
-                grid_layout.addWidget(self.salir, i, self.tamano + 1)
+                self.grid_layout.addWidget(self.salir, i, self.tamano + 1)
             elif i == 2:
-                grid_layout.addWidget(self.comprobar, i, self.tamano + 1)
+                self.grid_layout.addWidget(self.comprobar, i, self.tamano + 1)
             else:
                 vacio = QLabel("")
-                grid_layout.addWidget(vacio, i, self.tamano + 1)
-
-        self.cor_x = 0  # estas coordenadas indican exactamente dónde está pepe (pixeles)
-        self.cor_y = 0
-        self.xs = 1  # mientras que estas indican en que cuadrado está (matriz de lechugas de nxn)
-        self.ye = 1
-        self.pepa = QLabel(self)
-        self.pepa.setPixmap(QPixmap(p.RUTAS["abajo"][0]))
-        self.pepa.setGeometry(self.cor_x, self.cor_y, p.ANCHO_LECHUGA, p.ALTURA_LECHUGA)
-        self.pepa.setWindowFlags(self.pepa.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
-
-        self.thread = Thread()
-        self.thread.fin.connect(self.thread_fin)
-
-        self.mutex = QMutex()
-
-        QTimer.singleShot(100, self.definir_Pepa)
-
-    def definir_Pepa(self):
-        self.cor_x = self.layout().itemAtPosition(self.xs, self.ye).widget().geometry().x()
-        self.cor_y = self.layout().itemAtPosition(self.xs, self.ye).widget().geometry().y()
-        QTimer.singleShot(100, lambda: self.mover_final("abajo", 0))
-
-    def keyPressEvent(self, event: QKeyEvent) -> None:
-        if not self.thread.isRunning() and self.mutex.tryLock():
-            if event.key() == Qt.Key.Key_W:
-                if self.ye != 1:
-                    self.thread.start()
-                    self.mover_arriba()
-
-            if event.key() == Qt.Key.Key_S:
-                if self.ye != self.tamano:
-                    self.thread.start()
-                    self.mover_abajo()
-
-            if event.key() == Qt.Key.Key_A:
-                if self.xs != 1:
-                    self.thread.start()
-                    self.mover_izquierda()
-
-            if event.key() == Qt.Key.Key_D:
-                if self.xs != self.tamano:
-                    self.thread.start()
-                    self.mover_derecha()
-
-    def mover_abajo(self, paso=0):
-        if paso < 4:
-            self.cor_y += p.PASOS[self.dificultad]
-            self.mover_pepa("abajo", paso)
-            QTimer.singleShot(100, lambda: self.mover_abajo(paso + 1))
-        else:
-            self.xs += 1
-            QTimer.singleShot(100, lambda: self.mover_final("abajo", 0))
-            self.thread.fin.connect(self.thread_fin)
-
-    def mover_arriba(self, paso=0):
-        if paso < 4:
-            self.cor_y -= p.PASOS[self.dificultad]
-            self.mover_pepa("arriba", paso)
-            QTimer.singleShot(100, lambda: self.mover_arriba(paso + 1))
-        else:
-            self.xs -= 1
-            QTimer.singleShot(100, lambda: self.mover_final("abajo", 0))
-            self.thread.fin.connect(self.thread_fin)
-
-    def mover_izquierda(self, paso=0):
-        if paso < 4:
-            self.cor_x -= p.PASOS[self.dificultad]
-            self.mover_pepa("izquierda", paso)
-            QTimer.singleShot(100, lambda: self.mover_izquierda(paso + 1))
-        else:
-            self.ye -= 1
-            QTimer.singleShot(100, lambda: self.mover_final("abajo", 0))
-            self.thread.fin.connect(self.thread_fin)
-
-    def mover_derecha(self, paso=0):
-        if paso < 4:
-            self.cor_x += p.PASOS[self.dificultad]
-            self.mover_pepa("derecha", paso)
-            QTimer.singleShot(100, lambda: self.mover_derecha(paso + 1))
-        else:
-            self.ye += 1
-            QTimer.singleShot(100, lambda: self.mover_final("abajo", 0))
-            self.thread.fin.connect(self.thread_fin)
-
-    def mover_pepa(self, donde, imagen):
-        self.pepa.setPixmap(QPixmap(p.RUTAS[donde][imagen]))
-        self.pepa.move(self.cor_x, self.cor_y)
-
-    def mover_final(self, donde, imagen):
-        self.cor_x = self.layout().itemAtPosition(self.xs, self.ye).widget().geometry().x()
-        self.cor_y = self.layout().itemAtPosition(self.xs, self.ye).widget().geometry().y()
-        self.pepa.move(self.cor_x, self.cor_y)
-        self.pepa.setPixmap(QPixmap(p.RUTAS[donde][imagen]))
-        self.mutex.unlock()
-
-    def thread_fin(self):
-        self.thread.quit()
-
-    def retirada(self):
-        self.hide()
-        sys.exit()
+                self.grid_layout.addWidget(vacio, i, self.tamano + 1)
+        self.setLayout(self.grid_layout)
 
     def enviar_info(self):
         pass
+
+    def retirada(self):
+        self.hide()
+        self.signal_salir.emit()
+
+    def posiciones_pepa(self):
+        self.cor_x = self.layout().itemAtPosition(1, 1).widget().geometry().x()
+        self.cor_y = self.layout().itemAtPosition(1, 1).widget().geometry().y()
+        self.signal_posicion.emit([self.cor_x, self.cor_y])
+
+    def definir_Pepa(self):
+        self.cor_x = self.layout().itemAtPosition(1, 1).widget().geometry().x()
+        self.cor_y = self.layout().itemAtPosition(1, 1).widget().geometry().y()
+        self.pepa.move(self.cor_x, self.cor_y)
+
+    def instalar_atajos(self) -> None:
+        self.cheatcode_silenciar = QShortcut(QKeySequence("M, U, T, E"), self)
+        self.cheatcode_inf = QShortcut(QKeySequence("I, N, F"), self)
+        self.cheatcode_inf.activated.connect(self.tiempo_infinito)
+        self.cheatcode_silenciar.activated.connect(self.silenciar)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        if event.key() == Qt.Key.Key_W:
+            self.signal_arriba.emit()
+
+        if event.key() == Qt.Key.Key_S:
+            self.signal_abajo.emit()
+
+        if event.key() == Qt.Key.Key_A:
+            self.signal_izquierda.emit()
+
+        if event.key() == Qt.Key.Key_D:
+            self.signal_derecha.emit()
+
+    def silenciar(self):
+        self.signal_silenciar.emit()
+        print("me debería silenciar")
+
+    def tiempo_infinito(self):
+        tiempo_og = self.grid_layout.itemAtPosition(0, self.tamano + 1).widget()
+        if isinstance(tiempo_og, Tiempo):
+            tiempo_og.label2.setText("Te quedan ∞ segundos.")
+            tiempo_og.detener()
+
+
+if __name__ == '__main__':
+    def hook(type, value, traceback) -> None:
+        print(type)
+        print(traceback)
+    sys.__excepthook__ = hook
+
+    app = QApplication([])
+    # a = QFontDatabase.addApplicationFont(p.PATH_LETRA)
+    font = QFont("Cascadia Mono SemiBold", 10)
+    app.setFont(font)  # Creamos las base de la app: QApplication.
+    ventana = Tablero("experto_1.txt")
+    ventana2 = Tablero("novato_1.txt")   # Construimos un QWidget que será nuestra ventana.
+    ventana3 = Tablero("intermedio_1.txt")
+    ventana.show()
+    ventana3.show()  # Mostramos la ventana.
+    ventana2.show()
+    sys.exit(app.exec())
